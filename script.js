8ced61da8395f9ec47162086deecc775a9196550
NiiKoteyDjanie <sampson.kotey.djanie@gmail.com>
2024-03-03 15:30:05 +0000

    use querySelector() to find elements by id

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ currentWeapon = 0;
 fighting;
 monsterHealth;
 inventory = ["stick"]
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
