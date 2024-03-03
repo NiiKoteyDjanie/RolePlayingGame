c16f4c7bf8e77ab22ccf59f39a94275e74e5d78e
NiiKoteyDjanie <sampson.kotey.djanie@gmail.com>
2024-03-03 19:23:01 +0000

    functions

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let xp = 0;
-health = 100;
-gold = 50;
-currentWeapon = 0;
-fighting;
-monsterHealth;
-inventory = ["stick"]
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
 
-const button1 = document.querySelector("#button1");
+const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
@@ -15,3 +15,21 @@ const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+}
+
+function goCave() {
+
+}
+
+function fightDragon() {
+
+}
